97b682ef4c2e124dcf823f9bd7c15f063bfe179a
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-16 18:43:08 +0200

    cleaned up "app.module" by creating "shared.module" to store external modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LearnmoreComponent } from './learnmore/learnmore.component';
 import { MedicalFileComponent } from './patients/medical-file/medical-file.component';
 import { AppointmentsComponent } from './patients/appointments/appointments.component';
 import { MenuComponent } from './patients/menu/menu.component';
-import {ToastrModule} from 'ngx-toastr';
-
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -34,10 +31,7 @@ import {ToastrModule} from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
