3942d5db14b828102b961669f5d1b8c24561395e
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-16 18:18:41 +0200

    added angular route guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { MedicalFileComponent } from './patients/medical-file/medical-file.component';
 import { AppointmentsComponent } from './patients/appointments/appointments.component';
 import { MenuComponent } from './patients/menu/menu.component';
+import { authGuard } from './_guards/auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'menu', component: MenuComponent, canActivate: [authGuard]},
   {path: 'medicalfile', component: MedicalFileComponent},
   {path: 'appointments', component: AppointmentsComponent},
-  {path: 'menu', component: MenuComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
 
