05e21616763c9f89d7160166306349ee70e10a9c
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-20 14:51:43 +0200

    extended the "AppUser" entity  + created "Photo" entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,5 +8,13 @@ public class AppUser
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
-
+    public DateOnly DateOfBirth { get; set; }
+    public int MyProperty { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<Photo> Photos { get; set; } = new();
+    
 }
