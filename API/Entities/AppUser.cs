afac905465df489b5eae75a6cc1e09b7db7016bc
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-21 04:58:58 +0200

    edited "AppUser"

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,26 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
-using API.Extensions;
+﻿using API.Extensions;
 
-namespace API.Entities;
-
-public class AppUser
-{
-    public int Id { get; set; }
-    public string UserName { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
-    public DateOnly DateOfBirth { get; set; }
-    public int MyProperty { get; set; }
-    public DateTime Created { get; set; } = DateTime.UtcNow;
-    public DateTime LastActive { get; set; } = DateTime.UtcNow;
-    public string Gender { get; set; }
-    public string City { get; set; }
-    public string Country { get; set; }
-    public Photo Photo { get; set; }
-
-    public int GetAge()
+namespace API.Entities
+{ 
+    public class AppUser
     {
-        return DateOfBirth.CalculateAge();
-    }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public Photo Photo { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     
+    }
 }
