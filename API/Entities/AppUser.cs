b2ed54516b8caea371c2e5238810e582df582dce
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-21 04:43:31 +0200

    single use is better suited for "Photo" instead of "List<Photo>"

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -16,7 +16,7 @@ public class AppUser
     public string Gender { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public List<Photo> Photos { get; set; } = new();
+    public Photo Photos { get; set; }
 
     public int GetAge()
     {
