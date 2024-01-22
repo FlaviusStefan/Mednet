7a4ddd1c79d6209ccaa72179b048fcec1162d440
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-22 02:14:19 +0200

    added "Doctor" entity

---
+++ API/Entities/Doctor.cs
@@ -0,0 +1,25 @@
+using API.Extensions;
+
+namespace API.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Specialization { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new ();
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
+    }
+}
