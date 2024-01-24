a88768ce989c01901d5ad1a9ce7eb3ee3838431b
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-25 01:55:33 +0200

    fixed&upgraded AutoMapper functionality

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 
 namespace API.DTOs
 {
     public class MemberDto
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public int Age { get; set; }
         public DateTime Created { get; set; }
