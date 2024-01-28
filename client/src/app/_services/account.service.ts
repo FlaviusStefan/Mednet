ad5c69308b168884d4b9881199503616993097ba
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-01-28 20:00:43 +0200

    changed a bit of service logic: from user only to -> doctor & patient

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
 import { Doctor } from '../_models/doctor';
+import { Patient } from '../_models/patient';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,12 @@ export class AccountService {
   baseUrl = 'https://localhost:5003/api/';
   private currentUserSource = new BehaviorSubject<User | null>(null);
   private currentDoctorSource = new BehaviorSubject<Doctor | null>(null);
+  private currentPatientSource = new BehaviorSubject<Patient | null>(null);
+
   currentUser$ = this.currentUserSource.asObservable();
   currentDoctor$ = this.currentDoctorSource.asObservable();
+  currentPatient$ = this.currentPatientSource.asObservable();
+
 
   constructor(private http: HttpClient) { }
 
@@ -28,6 +33,30 @@ export class AccountService {
     )
   }
 
+  loginDoctor(model: any){
+    return this.http.post<Doctor>(this.baseUrl + 'account/logindoctor', model).pipe(
+      map((response: Doctor) => {
+        const doctor = response;
+        if (doctor) {
+          localStorage.setItem('doctor', JSON.stringify(doctor));
+          this.currentDoctorSource.next(doctor);
+        }
+      })
+    )
+  }
+
+  loginPatient(model: any){
+    return this.http.post<Patient>(this.baseUrl + 'account/loginpatient', model).pipe(
+      map((response: Patient) => {
+        const patient = response;
+        if (patient) {
+          localStorage.setItem('user', JSON.stringify(patient));
+          this.currentPatientSource.next(patient);
+        }
+      })
+    )
+  }
+
   register(model: any){
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user=> {
@@ -38,13 +67,32 @@ export class AccountService {
       })
     )
   }
+
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
+  }
+
+  setCurrentDoctor(doctor: Doctor) {
+    this.currentDoctorSource.next(doctor);
+  }
+
+  setCurrentPatient(patient: Patient) {
+    this.currentPatientSource.next(patient);
   }
 
   logout(){
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  logoutDoctor(){
+    localStorage.removeItem('doctor');
+    this.currentDoctorSource.next(null);
+  }
+
+  logoutPatient(){
+    localStorage.removeItem('patient');
+    this.currentPatientSource.next(null);
   }
 
 }
