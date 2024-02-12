7c227fcbe97a49992dd3ba2e0c8df9d4cabb7ba8
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2024-02-13 01:18:29 +0200

    merged 2 entity-services into a single one

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Patient } from '../_models/patient';
+import { Doctor } from '../_models/doctor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient){}
+
+  getPatients(){
+    return this.http.get<Patient[]>(this.baseUrl + 'patients', this.getHttpOptions())
+  }
+
+  getPatient(username: string){
+    return this.http.get<Patient>(this.baseUrl + 'patients/' + username, this.getHttpOptions())
+  }
+
+  getDoctors(){
+    return this.http.get<Doctor[]>(this.baseUrl + 'doctors')
+  }
+
+  getDoctor(username: string){
+    return this.http.get<Doctor>(this.baseUrl + 'doctors/' + username, this.getHttpOptions())
+  }
+
+  getHttpOptions(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    return{
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + user.token
+      })
+    }
+  }
+}
